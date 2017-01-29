640ed7f40c0bb26e2a434e61fd075ebd55bd6207
tuancaraballo <tranc2606@gmail.com>
2017-01-28 23:42:15 -0800

    find is a binary tree is a sum tree

---
+++ isSumBinaryTree.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+#include <limits>
+using namespace std;
+
+
+/*
+ |    Question: find if a tree is a sum tree. A sum tree is a type
+ |              of tree such that a node is the total sum of the nodes
+ |              below it: all its family: children and gran grand children
+ |
+ |  Approach#1: Print those nodes that don't have both children
+ |              or don't have one of the children.
+ |
+ */
+
+int negative_infinity = -INFINITY;
+
+
+struct node{
+    node * left = NULL;
+    node * right = NULL;
+    node(node * l, node * r, int dat){
+        left = l;
+        right = r;
+        data = dat;
+    }
+    int data;
+};
+
+
+
+
+
+void printInOrder(node * root){
+    if (!root) return;
+    
+    printInOrder(root->left);
+    cout<<root->data<<", ";
+    printInOrder(root->right);
+}
+
+int isSumTree(node * root){
+    
+    if(!root) return 0;
+    int sum_left = isSumTree(root->left);
+    int sum_right = isSumTree(root->right);
+    
+    if(sum_left + sum_right != root->data){
+        return negative_infinity;
+    }
+    
+    return sum_left + sum_right + root->data;
+}
+
+int main() {
+    
+    
+    node * first = new node(NULL,NULL,1);
+    
+    node * third = new node(NULL,NULL,3);
+    node * second= new node(first,third,2);
+    
+    
+    node * twnt = new node(NULL,NULL,20);
+    node * thir5 = new node(NULL,NULL, 35);
+    
+    node * fourty = new node(thir5,NULL,40);
+    node * thirty = new node(twnt,fourty,30);
+    
+    node * eighty = new node(NULL,NULL,80);
+    node * fifty = new node(thirty,eighty,50);
+    
+    
+    printInOrder(fifty);
+    cout<<endl;
+    
+    
+    
+    
+    
+    
+    return 0;
+}
